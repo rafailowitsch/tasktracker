817ad18d3b4f924c3eb744c96d907c08491a722b
rafailowitsch <rafailowitsch@proton.me>
2024-03-07 13:23:21 +0300

    Added user handler

--- internal/delivery/http/users.go
+++ internal/delivery/http/users.go
@@ -13,6 +13,8 @@ type userInput struct {
 
 func (h *Handler) SignUp(w http.ResponseWriter, r *http.Request) {
 	var input userInput
+	ctx := r.Context()
+
 	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
 		h.log.Error("Error decoding JSON", sl.Err(err))
 		http.Error(w, "Error deconding JSON", http.StatusBadRequest)
@@ -25,16 +27,56 @@ func (h *Handler) SignUp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_ = h.services.Users.SignUp(
-		r.Context(),
+	err := h.services.Users.SignUp(
+		ctx,
 		input.Name,
 		input.Password,
 	)
 
-	// if err != nil {
-	// }
+	if err != nil {
+		h.log.Error("Service layer error", sl.Err(err))
+		http.Error(w, "Service layer error", http.StatusInternalServerError)
+	}
 }
 
 func (h *Handler) SignIn(w http.ResponseWriter, r *http.Request) {
+	var input userInput
+	ctx := r.Context()
 
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		h.log.Error("Error decoding JSON", sl.Err(err))
+		http.Error(w, "Error deconding JSON", http.StatusBadRequest)
+		return
+	}
+
+	if input.Name == "" || input.Password == "" {
+		h.log.Error("The name or password field is empty")
+		http.Error(w, "One of the fields is empty", http.StatusBadRequest)
+		return
+	}
+
+	res, err := h.services.Users.SignIn(
+		ctx,
+		input.Name,
+		input.Password,
+	)
+
+	if err != nil {
+		h.log.Error("Service layer error", sl.Err(err))
+		http.Error(w, "Service layer error", http.StatusInternalServerError)
+	}
+
+	response := map[string]string{
+		"AccessToken":  res.AccessToken,
+		"RefreshToken": res.RefreshToken,
+	}
+
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		h.log.Error("Error encoding JSON")
+		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResponse)
 }
